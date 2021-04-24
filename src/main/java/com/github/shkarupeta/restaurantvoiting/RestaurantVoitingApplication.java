1db4465fc66565ab168890ed545b3ab0aaa2b244
Sergey Shkarupeta <shkarupeta.sergey@gmail.com>
2021-04-24 11:42:41 +0300

    create RestaurantVoitingApplication project

---
+++ src/main/java/com/github/shkarupeta/restaurantvoiting/RestaurantVoitingApplication.java
@@ -0,0 +1,14 @@
+package com.github.shkarupeta.restaurantvoiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class RestaurantVoitingApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVoitingApplication.class, args);
+    }
+
+}
